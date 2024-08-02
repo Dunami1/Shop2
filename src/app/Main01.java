283464aef6222e6cedbe34e910e2d84ae431b08c
Dima <dimamakushev2016@gmail.com>
2024-08-02 12:14:31 +0300

    Initial commit

---
+++ src/app/Main01.java
@@ -0,0 +1,29 @@
+package app;
+
+public class Main01 {
+
+    public static void main(String[] args) {
+
+        String product1 = "smartphone";
+        String product2 = "laptop";
+
+        double price1 = 305.99;
+        double price2 = 570.95;
+
+        String address1 = "Moon Street";
+        String address2 = "Terra Street";
+
+        int adr = 10;
+        int adr1 = 17;
+
+        System.out.println("Order № 1 Client: Alice." + "\nProduct: " + product1 + "," +
+                "\nprice: EUR " + price1 + "." + "\nAddress: " + address1 + ", " + adr + "." + "\nOrder № 2 Client: Tom."+
+                "\nProduct: " + product2 + "," + "\nprice: EUR " + price2 + "." + "\nAddress: " + address2 + ", " + adr1 + "." );
+
+
+
+
+    }
+
+
+}
